1fb4e9af0fd1365cd079614994b411f0e9598108
joobong7 <joobong7@tricycle.co.kr>
2018-12-05 17:51:17 +0900

    수정

--- Noah_Main/js/main.js
+++ Noah_Main/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     startTime();
     getpoemdata();
     getData();
+    //console.log(chrome.bookmarks);
+    
 
     var fixedlayerCookie = C_COOKIE.getCookie("fixedlayer");
     if (fixedlayerCookie != "" && fixedlayerCookie != null && fixedlayerCookie == true){
@@ -21,6 +23,13 @@ $(document).ready(function(){
             C_COOKIE.setCookie("fixedlayer" , !fixedlayerCookie , 1)
         })
     });
+
+    $(document).on("click" , "#btnChangeDesk" , function(){
+        $("#desck_1").toggle(500  );    
+        $("#desck_2").toggle(500 );
+    });
+
+
 
     $(document).on("click" , "#doingBtn" , function(){
         var doingtxt =  $("#doingTxt").val();
@@ -46,6 +55,27 @@ $(document).ready(function(){
         console.log(finish);
         getData();
     });
+
+    $(document).on("click" , "#btnGetNews" , function(){
+        var nCode = "xinhua-net";
+        nCode = "techcrunch-cn";
+        newsAPi(nCode);
+    })
+
+
+
+
+
+    // $("details").on("click", function() {
+    //     document.querySelectorAll('details').removeAttr('open');
+
+    //     $("details[open]")
+    //         .not(this)
+    //         .removeAttr("open");
+    // });
+
+
+
 });
 
 
@@ -120,14 +150,140 @@ async function getData(){
     var p_data  = await p_obj.GETALLDOC();
     console.log(p_data);
     $(".doinglist ul").empty();
+    
     p_data.forEach( item => {
         var html = "<li>"
-        + "<input type='text' value='" + item.doc.navigationData.n_title  +  "' >"
+        + "<input type='text' value='" + item.doc.navigationData.n_title  +  "' />"
+        + " <span>" +  getFormattedDate(item.doc.navigationData.n_regdate) + "</span>  "
         + "<button class='btn' data-dataid='" + item.id + "'>"
-        + "            <span>X</span>"
+        + "         <span>X</span>"
         + "</button>"
         + "</li>";
         $(".doinglist ul").append(html);
     });
 
+}
+
+
+
+function getFormattedDate(n_regdateStr) {
+    var n_date = new Date(n_regdateStr);
+    var year = n_date.getFullYear();
+  
+    var month = (1 + n_date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+  
+    var day = n_date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return month + "-" + day + "-" + year;
+  }
+
+  
+
+
+
+
+  var newsAPi = function( news_Type ){
+    var Init_key = "cce2ec7aae82464aa36126ae2e7f43bc";
+    var Init_Url = "https://newsapi.org/v2/everything";
+
+    //https://newsapi.org/v2/everything?sources=xinhua-net&apiKey=cce2ec7aae82464aa36126ae2e7f43bc
+    var makeCallUrl = Init_Url + "?sources=" + news_Type + "&apiKey=" + Init_key; 
+    //makeCallUrl = "https://newsapi.org/v2/everything?sources=xinhua-net&apiKey=cce2ec7aae82464aa36126ae2e7f43bc";
+    var param = {  };
+    
+    var ajaxobj = new fnAjaxObj();
+    news_1(makeCallUrl);
+    // ajaxobj.fnajax( makeCallUrl ,param , function(res){
+    //     rendernewsHtml(res);
+    //     console.log(res);
+    // });
+  }
+  var rendernewsHtml = function(obj){
+    if(obj.length <= 0){
+        return;
+    }
+    $(".newsContent div").empty();
+    obj.articles.forEach( item => {
+        var html = "<details>"
+        //+ " <a href='" + item.url  + "' target='_blank' >"
+        + " <summary>" + item.title  +  "</summary>"
+        + " <span>" +  getFormattedDate(item.publishedAt) + "</span>  "
+        //+ " </a>"
+        // + " <button class='btn' data-dataid='" + item.id + "'>"
+        // + "         <span>X</span>"
+        // + " </button>"
+        + "<p>" + item.description + "</p>"
+        + "</details>";
+        $(".newsContent div").append(html);
+    });
+  }
+
+
+function news_1( n_url ){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                let articles = JSON.parse(xmlhttp.responseText);
+                console.log(articles);
+                rendernewsHtml(articles);
+            }
+        }
+    };
+    xmlhttp.open("GET", n_url , true);
+    xmlhttp.send();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+var fnAjaxObj = function(){
+    this.fnAjaxCall = false;
+}
+fnAjaxObj.prototype.fnajax =  function( url , param , fnCallback ){
+    fnObj = this.fnAjaxCallback ;
+    if(typeof fnCallback != "undefined"){
+        fnObj = fnCallback;
+    }
+    if(url == ""){
+        alert('불정확한 호출입니다.');
+        return;
+    }
+    $.ajax({
+        type: 'GET',
+        url: url ,
+        data: param,
+        dataType: 'jsonp', 
+        jsonpCallback: "myCallback",
+        beforeSend: function (jqXHR, opts) {
+            //전송중일때
+            if (this.fnAjaxCall) {
+                jqXHR.abort();
+                alert("응모중입니다.");
+            }
+            else { //전송중이 아닐때
+                this.fnAjaxCall = true;
+            }
+        },
+        success:  fnObj ,
+        complete: function (results) {
+            this.fnAjaxCall = false;
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('서버와 통신중 에러가 발생했습니다. ' + errorThrown);
+        }
+    });
+}
+fnAjaxObj.prototype.fnAjaxCallback = function(res){
+    console.log(res);
 }
